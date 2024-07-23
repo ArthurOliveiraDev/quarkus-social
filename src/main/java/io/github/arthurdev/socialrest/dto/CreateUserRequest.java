d565f8d89fbc9e40dc207ddcb6374a15e211fcd1
ArthurOliveiraDev <arthuroliveirasouza.dev@gmail.com>
2024-07-23 19:19:33 -0300

    commit inicial

---
+++ src/main/java/io/github/arthurdev/socialrest/dto/CreateUserRequest.java
@@ -0,0 +1,16 @@
+package io.github.arthurdev.socialrest.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+
+@Data
+public class CreateUserRequest {
+
+    @NotBlank(message = "name is required")
+    private String name;
+    @NotNull(message = "age is required")
+    private Integer age;
+
+}
---
+++ src/main/java/io/github/arthurdev/socialrest/dto/ResponseError.java
@@ -0,0 +1,36 @@
+package io.github.arthurdev.socialrest.dto;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.ws.rs.core.Response;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ResponseError {
+
+    public static final int UNPROCESSABLE_ENTITY_STATUS = 422; 
+    private String message;
+    private Collection<FieldError> errors;
+
+    public static <T> ResponseError createFromValidation(Set<ConstraintViolation<T>> violations) {
+        List<FieldError> errors = violations
+                .stream()
+                .map(cv -> new FieldError(cv.getPropertyPath().toString(), cv.getMessage()))
+                .collect(Collectors.toList());
+
+        String message = "validation error";
+
+        var responseError = new ResponseError(message, errors);
+        return responseError;
+    }
+
+    public Response withStatusCode(int code) {
+        return Response.status(code).entity(this).build();
+    }
+}
